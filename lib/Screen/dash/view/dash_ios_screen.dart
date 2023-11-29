dd09007a7aad06ad1cdb84fdaf86308274f2d827
os <kkeval304@gmail.com>
2023-11-29 09:47:09 +0530

    new4

--- lib/Screen/dash/view/dash_ios_screen.dart
+++ lib/Screen/dash/view/dash_ios_screen.dart
@@ -20,42 +20,38 @@ class _DashIosScreenState extends State<DashIosScreen> {
     ChatsIosScreen(),
     CallIosScreen(),
   ];
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabView(
-      builder: (BuildContext context) {
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              IndexedStack(
-                index: providerw!.stepIndex,
-                children: screens,
-              ),
-              CupertinoTabBar(
-                currentIndex: providerr!.stepIndex,
-                onTap: (value) {
-                  int i = value;
-                  providerr!.changeStep(i);
-                },
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.person_add),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.chat_bubble_text),
-                    label: 'Contact',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.phone),
-                    label: 'Contact',
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
-    );
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          currentIndex: providerr!.stepIndex, onTap: (value) {
+          int i = value;
+          providerr!.changeStep(i);
+        },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person_add),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble_text),
+              label: 'Contact',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.phone),
+              label: 'Contact',
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(
+           builder: (context) {
+             return screens[
+               providerw!.stepIndex
+             ];
+           },
+          );
+        },);
   }
 }
